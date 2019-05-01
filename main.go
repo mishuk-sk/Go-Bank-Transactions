46f21c689cdbffbac00f26d6b695044ffd4a586f
mishuk <mishuk.sk@live.ru>
2019-05-01 16:53:25 +0300

    added lib/pq to vendor

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// User type defines user object representation
+type User struct {
+	ID         string `json:"id"`
+	FirstName  string `json:"first_name"`
+	SecondName string `json:"second_name"`
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
+}
+
 func main() {
+	router := mux.NewRouter()
+	subRouter := router.PathPrefix("/users").Subrouter()
+
+	subRouter.HandleFunc("/", ListUsers).Methods(http.MethodGet)
+	subRouter.HandleFunc("/", CreateUser).Methods(http.MethodPost)
+}
+
+func ListUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 }
