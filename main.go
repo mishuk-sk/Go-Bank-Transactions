7b2f6bc749c1bb7d202ca7387c737d70fcac8f00
mishuk <mishuk.sk@live.ru>
2019-05-18 12:46:20 +0300

    initial commit

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
+
+type configuration struct {
+	DbName     string `json:"db_name"`
+	DbPort     int    `json:"db_port"`
+	DbHost     string `json:"db_host"`
+	DbUser     string `json:"db_user"`
+	DbPassword string `json:"db_password"`
+}
 
 // User type defines user object representation
 type User struct {
@@ -16,6 +29,23 @@ type User struct {
 }
 
 func main() {
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		panic(err)
+	}
+	config := configuration{}
+	err = json.NewDecoder(configFile).Decode(&config)
+	if err != nil {
+		panic(err)
+	}
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
+		config.DbHost, config.DbPort, config.DbUser, config.DbPassword, config.DbName)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 	router := mux.NewRouter()
 	subRouter := router.PathPrefix("/users").Subrouter()
 
