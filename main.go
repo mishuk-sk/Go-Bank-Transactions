d7c82d284a8779d4ba541cd1a6f7cbed8ecb40fb
mishuk <mishuk.sk@live.ru>
2019-05-19 21:20:31 +0300

    added operations on user

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -20,53 +23,71 @@ type configuration struct {
 	DbUser     string `json:"db_user"`
 	DbPassword string `json:"db_password"`
 }
+type User struct {
+	ID         uuid.UUID   `json:"id"`
+	FirstName  string      `json:"first_name"`
+	SecondName string      `json:"second_name"`
+	Phone      interface{} `json:"phone"`
+	Email      interface{} `json:"email"`
+}
 
 var db *sql.DB
 
-// User type defines user object representation
-type User struct {
-	ID         string `json:"id"`
-	FirstName  string `json:"first_name"`
-	SecondName string `json:"second_name"`
-	Phone      string `json:"phone"`
-	Email      string `json:"email"`
-}
-
 func main() {
+	var wait time.Duration
+	config := configuration{}
+
 	configFile, err := os.Open("config.json")
 	if err != nil {
 		panic(err)
 	}
-	config := configuration{}
 	err = json.NewDecoder(configFile).Decode(&config)
 	if err != nil {
 		panic(err)
 	}
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		config.DbHost, config.DbPort, config.DbUser, config.DbPassword, config.DbName)
-
 	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+	// test db connection
+	if err := db.Ping(); err != nil {
+		log.Printf("%v\n", err)
+	} else {
+		log.Println("Db successfully connected")
+	}
+
+	// initializing routes
 	router := mux.NewRouter()
-	subRouter := router.PathPrefix("/users").Subrouter()
 	router.HandleFunc("/", checkLive).Methods(http.MethodGet)
-	subRouter.HandleFunc("/", ListUsers).Methods(http.MethodGet)
-	subRouter.HandleFunc("/", CreateUser).Methods(http.MethodPost)
+	usersRouter := router.PathPrefix("/users").Subrouter()
+	usersRouter.HandleFunc("/", ListUsers).Methods(http.MethodGet)
+	usersRouter.HandleFunc("/", CreateUser).Methods(http.MethodPost)
+	usersRouter.HandleFunc("/{id}", UpdateUser).Methods(http.MethodPut)
+	usersRouter.HandleFunc("/{id}", DeleteUser).Methods(http.MethodDelete)
+	usersRouter.HandleFunc("/{id}", GetUser).Methods(http.MethodGet)
+
+	// http server
 	srv := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: router,
 	}
-	// tests
-	log.Printf("%v\n", db)
-	log.Printf("%v\n", db.Ping())
 
-	fmt.Println("Server started")
-	if err := srv.ListenAndServe(); err != nil {
-		log.Println(err)
-	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	log.Println("shutting down")
+	os.Exit(0)
 
 }
 
@@ -74,11 +95,96 @@ func checkLive(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("LIVE!!!"))
 }
 
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	usr, err := getUser(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	if err := json.NewDecoder(r.Body).Decode(&usr); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	if _, err := db.Exec("UPDATE users SET first_name = $1, second_name = $2, phone = $3, email = $4 WHERE id=$5", usr.FirstName, usr.SecondName, usr.Phone, usr.Email, usr.ID); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Successfully updated")
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	if _, err := db.Exec("DELETE FROM users WHERE id=$1", id); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	user, err := getUser(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	marshaledUser, err := json.Marshal(user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		log.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(marshaledUser)
+}
+
+func getUser(id uuid.UUID) (User, error) {
+	user := User{}
+	row := db.QueryRow("SELECT * FROM users WHERE id=$1", id)
+	if err := row.Scan(&user.ID, &user.FirstName, &user.SecondName, &user.Phone, &user.Email); err != nil {
+		return user, fmt.Errorf("%s\n", "User doesn't exist")
+	}
+	return user, nil
+}
+
 func ListUsers(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * FROM users")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "%s", "Something wrong with db")
+		fmt.Fprintf(w, "%s", "Can't connect to db")
+		log.Println(err)
 		return
 	}
 	defer rows.Close()
@@ -87,34 +193,40 @@ func ListUsers(w http.ResponseWriter, r *http.Request) {
 		usr := User{}
 		if err := rows.Scan(&usr.ID, &usr.FirstName, &usr.SecondName, &usr.Phone, &usr.Email); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "%s", "Something wrong with db")
+			fmt.Fprintf(w, "%s", "problem scanning row")
+			log.Println(err)
 			return
 		}
 		users = append(users, usr)
 	}
-	w.WriteHeader(http.StatusInternalServerError)
+
 	marshaledUsers, err := json.Marshal(users)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "%s", "Something wrong with marshalling")
+		log.Println(err)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 	w.Write(marshaledUsers)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	var user User
+	user := User{}
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Internal Server Error")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Wrong payload format")
+		log.Println(err)
 		return
 	}
-	id := uuid.New()
-	_, err := db.Exec("INSERT INTO users(id, first_name, second_name, phone, email) VALUES ($1, $2, $3, $4, $5)", id, user.FirstName, user.SecondName, user.Phone, user.Email)
-	if err != nil {
+	user.ID = uuid.New()
+
+	if _, err := db.Exec("INSERT INTO users(id, first_name, second_name, phone, email) VALUES ($1, $2, $3, $4, $5)", user.ID, user.FirstName, user.SecondName, user.Phone, user.Email); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "%s", "can't insert into db")
+		fmt.Fprintf(w, "%s", "Can't insert into db")
+		log.Println(err)
 		return
 	}
-	w.Write([]byte("Inserted"))
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s %v\n", "Inserted", user)
 }
