8b3c7ddf42e5a324a078b2529b96ad9caccd9b6d
mishuk <mishuk.sk@live.ru>
2019-05-20 16:30:21 +0300

    started refactoring

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -28,8 +27,8 @@ func Init(router *mux.Router, database *sqlx.DB) {
 	usersRouter := router.PathPrefix("/users").Subrouter()
 	usersRouter.HandleFunc("/", ListUsers).Methods(http.MethodGet)
 	usersRouter.HandleFunc("/", CreateUser).Methods(http.MethodPost)
-	/*usersRouter.HandleFunc("/{id}", UpdateUser).Methods(http.MethodPut)
-	usersRouter.HandleFunc("/{id}", DeleteUser).Methods(http.MethodDelete)
+	usersRouter.HandleFunc("/{id}", UpdateUser).Methods(http.MethodPut)
+	/*usersRouter.HandleFunc("/{id}", DeleteUser).Methods(http.MethodDelete)
 	usersRouter.HandleFunc("/{id}", GetUser).Methods(http.MethodGet)
 	usersRouter.HandleFunc("/{id}/accounts", GetUserAccounts).Methods(http.MethodGet)
 	*/
@@ -56,28 +55,25 @@ func ListUsers(w http.ResponseWriter, r *http.Request) {
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	user := User{}
-	id := uuid.New()
+	user.ID = uuid.New()
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		raiseErr(err, w, http.StatusBadRequest)
 		return
 	}
-	rows, err := db.Query("INSERT INTO users(id, first_name, second_name, phone, email) VALUES($1, $2, $3, $4, $5)", id, user.FirstName, user.SecondName, user.Phone, user.Email)
-	if err != nil {
+	if _, err := db.Exec("INSERT INTO users(id, first_name, second_name, phone, email) VALUES($1, $2, $3, $4, $5)", user.ID, user.FirstName, user.SecondName, user.Phone, user.Email); err != nil {
 		raiseErr(err, w, http.StatusInternalServerError)
 		return
 	}
-	defer rows.Close()
-	rows.Next()
-	user, _ = scanUser(rows)
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(user)
 }
 
-// TODO is it arguable to define scanUser?
-func scanUser(row *sql.Rows) (User, error) {
-	user := User{}
-	if err := row.Scan(&user.ID, &user.FirstName, &user.SecondName, &user.Phone, &user.Email); err != nil {
-		return User{}, err
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(mux.Vars(r)["id"])
+	if err != nil {
+		raiseErr(err, w, http.StatusBadRequest)
+		return
 	}
-	return user, nil
+	user := User{}
+
 }
