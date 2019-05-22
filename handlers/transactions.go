73732c9c681d31444b54972bc9a11601be310837
mishuk <mishuk.sk@live.ru>
2019-05-22 14:53:46 +0300

    added debit transaction

--- handlers/transactions.go
+++ handlers/transactions.go
@@ -76,6 +76,7 @@ func AddTransaction(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(transaction)
 }
+
 func EnrichAccount(w http.ResponseWriter, r *http.Request) {
 	req := RequestTransaction{}
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
@@ -104,6 +105,39 @@ func EnrichAccount(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(transaction)
 }
+
+func DebitAccount(w http.ResponseWriter, r *http.Request) {
+	req := RequestTransaction{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		raiseErr(err, w, http.StatusBadRequest)
+		return
+	}
+	from_account, err := fetchAccount(mux.Vars(r)["account_id"])
+	if err != nil {
+		raiseErr(err, w, http.StatusBadRequest)
+		return
+	}
+	if from_account.Balance < req.Money {
+		raiseErr(fmt.Errorf("%s", "Not enough money on account"), w, http.StatusBadRequest)
+		return
+	}
+	transaction := Transaction{uuid.New(), time.Now(), from_account.ID, req}
+	tx, err := db.Beginx()
+	if err != nil {
+		raiseErr(err, w, http.StatusInternalServerError)
+		return
+	}
+	tx.Exec("UPDATE personal_accounts SET balance = balance - $1 WHERE id=$2", req.Money, from_account.ID)
+	tx.NamedExec("INSERT INTO transactions(id, date, from_account, to_account, money) VALUES(:id, :date, :from_account, :to_account, :money)", transaction)
+	if err := tx.Commit(); err != nil {
+		raiseErr(fmt.Errorf("%s, ERROR:%s", "Can't create transaction", err.Error()), w, http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(transaction)
+}
+
 func checkAccount(id uuid.UUID) bool {
 	var exists bool
 	if err := db.QueryRow("SELECT exists (SELECT id FROM personal_accounts WHERE id=$1)", id).Scan(&exists); err != nil && err != sql.ErrNoRows {
